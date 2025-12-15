c6e0a2e463c615e69aa6d9d3ee20fef765060356
Satish <sd1761256@gmail.com>
2025-12-15 19:59:11 +0530

    added games page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import Login from "./pages/Login"
 import  Register from "./pages/Register"
 import { Route,BrowserRouter,Router } from "react-router-dom"
 import Articles from "./pages/Articles"
+import Games from "./pages/Games"
 function App() {
   
   return (
     <BrowserRouter>
-      <Articles />
+      <Games />
       
     </BrowserRouter>
     
